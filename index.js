5a2762c54bcab5f5b7c5c1883472a6b78a20ab39
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-03-16 14:16:54 +0500

    Change port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
